d865d4f3d2d137ae212bad185f9fd9cd6f27868a
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-08-03 10:55:17 +0100

    Add in code to build a project if a build isn't provided (allows KIF tests to be run)

--- lib/hive/worker/ios.rb
+++ lib/hive/worker/ios.rb
@@ -25,15 +25,55 @@ module Hive
         super(device)
       end
 
+      def alter_project(project_path)
+        project = File.read(project_path)
+
+        project = replace_dev_team(project, 'PPH9EJ7977')
+        project = replace_code_sign_identity(project, 'iPhone Developer')
+        project = replace_provisioning_profile(project, '')
+
+        File.write(project_path, project)
+      end
+
+      def replace_project_data(options = {})
+        regex = Regexp.new(options[:regex])
+        replacements = options[:data].scan(regex).uniq.flatten
+
+        result = options[:data]
+        replacements.each do |to_replace|
+          result = result.gsub(to_replace, options[:new_value])
+        end
+
+        result
+      end
+
+      def replace_dev_team(project_data, new_dev_team)
+        replace_project_data(regex: '.*DevelopmentTeam = (.*);.*', data: project_data, new_value: new_dev_team )
+      end
+
+      def replace_code_sign_identity(project_data, new_identity)
+        replace_project_data(regex: '.*CODE_SIGN_IDENTITY.*= "(.*)";.*', data: project_data, new_value: new_identity)
+      end
+
+      def replace_provisioning_profile(project_data, new_profile)
+        replace_project_data(regex: '.*PROVISIONING_PROFILE = "(.*)";.*', data: project_data, new_value: new_profile)
+      end
+
       def pre_script(job, file_system, script)
         Hive.devicedb('Device').poll(@options['id'], 'busy')
 
-        FileUtils.mkdir(file_system.home_path + '/build')
-        app_path = file_system.home_path + '/build/' + 'build.ipa'
-        file_system.fetch_build(job.build, app_path) if job.build
+        if job.build
+          FileUtils.mkdir(file_system.home_path + '/build')
+          app_path = file_system.home_path + '/build/' + 'build.ipa'
 
-        app_info = DeviceAPI::IOS::Plistutil.get_bundle_id_from_app(app_path)
-        app_bundle = app_info['CFBundleIdentifier']
+          file_system.fetch_build(job.build, app_path)
+          app_info = DeviceAPI::IOS::Plistutil.get_bundle_id_from_app(app_path)
+          app_bundle = app_info['CFBundleIdentifier']
+          script.set_env 'BUNDLE_ID', app_bundle
+        else
+          # Change the profiles to
+          alter_project(file_system.home_path + '/test_code/code/PickNMix/PickNMix.xcodeproj/project.pbxproj')
+        end
 
         ip_address = DeviceAPI::IOS::IPAddress.address(self.device['serial'])
 
@@ -47,7 +87,6 @@ module Hive
 
         script.set_env 'APP_PATH', app_path
 
-        script.set_env 'BUNDLE_ID', app_bundle
         script.set_env 'DEVICE_TARGET', self.device['serial']
         script.set_env 'DEVICE_ENDPOINT', "http://#{ip_address}:37265"
 
