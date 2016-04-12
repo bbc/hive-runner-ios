d4885cb56ea7e7e3a1f6452aebe712ef780eb25a
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-04-12 10:04:34 +0100

    Removed references to DeviceDB

--- lib/hive/controller/ios.rb
+++ lib/hive/controller/ios.rb
@@ -6,70 +6,7 @@ module Hive
   class Controller
     class Ios < Controller
 
-      def register_with_devicedb
-        devices = DeviceAPI::IOS.devices
-        Hive.logger.debug('DDB: No devices attached') if devices.empty?
-
-        hive_details = Hive.devicedb('Hive').find(Hive.id)
-
-        attached_devices = []
-
-        if hive_details.key?('devices')
-          @hive_details = hive_details
-        else
-          hive_details = @hive_details
-        end
-
-        if hive_details.is_a? Hash
-          hive_details['devices'].select { |a| a['os'] == 'ios'}.each do |device|
-            registered_device = devices.select { |a| a.serial == device['serial'] && a.trusted? }
-            if registered_device.empty?
-              # A previously registered device isn't attached
-              Hive.devicedb('Device').hive_disconnect(device['id'])
-            else
-              Hive.devicedb('Device').poll(device['id'])
-
-              devices = devices - registered_device
-
-              begin
-                attached_devices <<
-                    self.create_device(device.merge(
-                        'os_version' => registered_device[0].version,
-                        'model' => device['device_model'],
-                        'device_range' => registered_device[0].device_class,
-                        'queues' => device['device_queues'].map{ |d| d['name'] },
-                        'queue_prefix' => @config['queue_prefix']
-                                       ))
-              rescue => e
-                Hive.logger.warn("Error with connected device: #{e.message}")
-              end
-            end
-          end
-          devices.select {|a| a.trusted? }.each do |device|
-            register_new_device(device)
-          end
-        else
-          # DeviceDB isn't available, use DeviceAPI instead
-          device_info = devices.select { |a| a.trusted? }.map do |device|
-            {
-                'id' => device.serial,
-                'serial' => device.serial,
-                'status' => 'idle',
-                'model' => device.model,
-                'brand' => 'Apple',
-                'os_version' => device.version,
-                'queue_prefix' => @config['queue_prefix']
-            }
-          end
-
-          attached_devices = device_info.collect do |physical_device|
-            self.create_device(physical_device)
-          end
-        end
-        attached_devices
-      end
-
-      def register_with_hivemind
+      def register
         devices = DeviceAPI::IOS.devices
         Hive.logger.debug('HM: No devices attached') if devices.empty?
 
@@ -138,7 +75,11 @@ module Hive
           end
 
           attached_devices = device_info.collect do |physical_device|
-            self.create_device(physical_device)
+            begin
+              self.create_device(physical_device)
+            rescue => e
+              Hive.logger.info("HM: Could not created device: #{physical_device}");
+            end
           end
         end
 
@@ -147,8 +88,7 @@ module Hive
       end
 
       def detect
-        register_with_hivemind
-        register_with_devicedb
+        register
       end
 
       def display_untrusted
@@ -158,48 +98,6 @@ module Hive
         return if untrusted_devices.empty?
         puts Terminal::Table.new headings: ['Untrusted devices'], rows: [untrusted_devices]
       end
-
-      def register_new_device(device)
-        Hive.logger.debug("Adding new iOS device: #{device.model}")
-
-        attributes = {
-            os:           'ios',
-            os_version:   device.version,
-            serial:       device.serial,
-            device_type:  'mobile',
-            device_model: device.model.to_s.gsub(',','_'),
-            device_range: device.device_class,
-            device_brand: 'Apple',
-            hive:         Hive.id
-        }
-
-        registration = Hive.devicedb('Device').register(attributes)
-        Hive.devicedb('Device').hive_connect(registration['id'], Hive.id)
-      end
-
-      def populate_queues(device)
-        queues = calculate_queue_names(device)
-
-        devicedb_queues = device['device_queues'].map { |d| d['name'] }
-        # Check to see if the queues have already been registered with this device
-        missing_queues = (queues - devicedb_queues) + (devicedb_queues - queues)
-        return if missing_queues.empty?
-
-        queues << missing_queues
-
-        queue_ids = queues.flatten.uniq.map { |queue| find_or_create_queue(queue) }
-
-        values = {
-            name: device['name'],
-            hive_id: device['hive_id'],
-            feature_list: device['features'],
-            device_queue_ids: queue_ids
-        }
-
-        Hive.devicedb('Device').edit(device['id'], values)
-      end
-
-
     end
   end
 end
